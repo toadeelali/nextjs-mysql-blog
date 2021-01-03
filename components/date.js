a7e199755318291f332ed503b5d24cc7ab74c9d4
Adeel Ali <wikz@live.com>
2021-01-04 00:30:02 +0500

    fix comments

--- components/date.js
+++ components/date.js
@@ -4,7 +4,5 @@ import utc from 'dayjs/plugin/utc';
 dayjs.extend(utc);
 
 export default function Date({ dateString }) {
-  // const date = parseISO(dateString)
-  // return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
   return <div>{dayjs(dayjs.utc(dateString).local()).format('MMMM DD, YYYY')}</div>;
 }
